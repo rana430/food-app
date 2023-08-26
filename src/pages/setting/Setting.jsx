1bf826b8e20a4fcaba981c9183455c3789dfc26f
Rana Wahid <rana20479@feng.bu.edu.eg>
2023-08-26 21:15:47 +0300

    #

--- src/pages/setting/Setting.jsx
+++ src/pages/setting/Setting.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./settings.css";
-
+import {FaIdBadge} from "react-icons/fa6";
 const Setting = () => {
   return (
     <div className="setting-container">
@@ -8,11 +8,11 @@ const Setting = () => {
         <h3>Settings</h3>
         <div className="settings-items">
           <ul>
-            <li>Personal Information</li>
-            <li>Menu Management</li>
-            <li>Order Management</li>
-            <li>Security&Privacy</li>
-            <li>Payment &Pricing</li>
+            <li className="active-li"><FaIdBadge className="icons"/>Personal Information</li>
+            <li><FaIdBadge className="icons"/>Menu Management</li>
+            <li><FaIdBadge className="icons"/>Order Management</li>
+            <li><FaIdBadge className="icons"/>Security&Privacy</li>
+            <li><FaIdBadge className="icons"/>Payment &Pricing</li>
           </ul>
         </div>
       </div>
