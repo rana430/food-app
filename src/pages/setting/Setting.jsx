60ed1c0e0157cbc527a9b4d26c081d5e1ec48735
Rana Wahid <ranawahid34@gmail.com>
2023-08-26 21:26:47 +0300

    Update Setting.jsx

--- src/pages/setting/Setting.jsx
+++ src/pages/setting/Setting.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import "./settings.css";
+import {FaIdBadge} from "react-icons/fa6";
 const Setting = () => {
   return (
-    <div>Setting</div>
-  )
-}
+    <div className="setting-container">
+      <div className="settings-sidebar">
+        <h3>Settings</h3>
+        <div className="settings-items">
+          <ul>
+            <li className="active-li"><FaIdBadge className="icons"/>Personal Information</li>
+            <li><FaIdBadge className="icons"/>Menu Management</li>
+            <li><FaIdBadge className="icons"/>Order Management</li>
+            <li><FaIdBadge className="icons"/>Security&Privacy</li>
+            <li><FaIdBadge className="icons"/>Payment &Pricing</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Setting
+export default Setting;
