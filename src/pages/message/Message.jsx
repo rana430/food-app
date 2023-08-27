493ca4dc2e850f727fafdf8f70a9dbe11b558205
Ahmed-Magdy-Talaat <85371544+Ahmed-Magdy-Talaat@users.noreply.github.com>
2023-08-27 13:32:36 +0300

    Update Message.jsx

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,19 +1,26 @@
-import React ,{ useEffect } from 'react'
-import Chatapp from './chatapp'
+import React from "react";
+import Chatapp from "./chatapp";
+import "./styles/Chatappstyle.css";
+import { useEffect } from "react";
 
 const Message = () => {
-  const Message = () => {
   useEffect(() => {
     // Set the overflow property on the body element
     document.body.style.overflow = "hidden";
+
+    // Clean up the effect when the component unmounts
     return () => {
       document.body.style.overflow = "auto"; // Reset to default
     };
   }, []);
-
   return (
-    <Chatapp/>
-  )
-}
+    <div
+      className="container-chats"
+      style={{ overflow: "hidden", height: "100vh" }}
+    >
+      <Chatapp />
+    </div>
+  );
+};
 
-export default Message
+export default Message;
