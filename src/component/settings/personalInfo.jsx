a04c14844586234bc0068771ce9064342de66277
Rana Wahid <rana20479@feng.bu.edu.eg>
2023-08-28 17:42:20 +0300

    settings

--- src/component/settings/personalInfo.jsx
+++ src/component/settings/personalInfo.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import  '../../css/customerstyles.css'
+import React from "react";
+import "../../css/customerstyles.css";
+
 export const PersonalInfo = () => {
   return (
-    <div className='setting-personal-info'></div>
-  )
-}
+    <div className="setting-personal-info">
+      <div className="upload-photo-flex">
+        <h6>Your Profile Picture</h6>
+        <div className="setting-upload-photo">
+          <p>Upload your Photo</p>
+          <img src="../../images/material-symbols_add-photo-alternate-outline.png" alt="" />
+
+        </div>
+      </div>
+    </div>
+  );
+};
